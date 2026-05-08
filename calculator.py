d2f384203939ed9a27cba852649bdd623a17c2e7
Saulo Roberto Oliveira Satyro <sauloroberto317@gmail.com>
2026-05-08 10:06:29 -0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+
+num1 = float(input("Insert a number: "))
+op = str(input("Insert a operator: "))
+num2 = float(input("Insert a operator: "))
+
+if op == '*' :
+    result = num1 * num2
+
+if op == '/' :
+    result = num1 / num2
+
+if op == '+' :
+    result = num1 + num2
+
+if op == '-':
+    result = num1 - num2
+
+print(result)
